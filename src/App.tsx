972dd2f5b3b30aed2d12e834ba235a6229d0d1c6
Jeremy Kimball <jeremyck54@gmail.com>
2024-05-09 16:47:21 -0600

    basic functional todo app finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
 import FormInput from "./components/FormInput";
+import { useState } from "react";
 import "./App.css";
 
+const initialValue: string[] = ["Example 1", "Example 2"];
+
 function App() {
+  const [formData, setFormData] = useState(initialValue);
+  
+  function handleFormData(data)
+  {
+    setFormData([...formData, data]);
+  }
+
+  function handleClick(index: number)
+  {
+    const updatedFormData = formData.filter((item, i) => i !== index);
+    setFormData(updatedFormData);
+  }
+  
   return (
     <>
       <h1>Todo</h1>
-      <FormInput />
+      <FormInput sendFormData={handleFormData} />
+      <ul>
+        {formData.map((item, index) => (
+        <li key={index} onClick={() => handleClick(index)}>{item}</li>
+        ))}
+      </ul>
     </>
   );
 }
--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -1,11 +1,18 @@
 import './FormInput.css'
+import { useState } from "react";
 
-export default function FormInput() {
+export default function FormInput({ sendFormData }) {
+  const [data, setData] = useState("");
+  function handleSubmit(event) {
+    event.preventDefault();
+    sendFormData(data);
+    setData("")
+  }
   return (
     <>
-      <form>
-        <input className="WriteTodo" type="text"/>
-        <input className="Submit" type="submit"></input>
+      <form method='post' onSubmit={handleSubmit}>
+        <input className="WriteTodo" type="text" value={data} onChange={(e) => setData(e.target.value)}></input>
+        <input className="Submit" type="submit" onSubmit={handleSubmit}></input>
       </form>
     </>
   );
